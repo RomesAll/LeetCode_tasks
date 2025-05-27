d0b9940d9f5e579c2db5943f2e45501191847b2a
Roman Beskokotov <roma@gmail.com>
2025-05-27 19:37:08 +0300

    result_commit

---
+++ Easy/two_sum_1.py
@@ -0,0 +1,32 @@
+class Solution:
+    
+    def twoSum(self, nums:list, target: int) -> list[int]:
+        
+        sum_index = []
+
+        if self.check_condition(nums, target):
+            srez = None
+            for i,v in enumerate(nums):
+                if v > target:
+                    srez = i
+                    break
+            
+            if srez and 0 <= srez <= len(nums):
+                nums = nums[:srez]
+
+            for i in range(0, len(nums)):
+                if 0 <= i+1 <= len(nums)-1:
+                    if nums[i] + nums[i+1] == target:
+                        sum_index.extend([i, i+1])
+                    
+        return sum_index
+
+    def check_condition(self, nums:list, target: int) -> bool:
+        
+        if 2 <= len(nums) <= 10**4 or -10**9 <= target <= 10**9:
+            for index, value in enumerate(nums,1):
+                if not(-10**9 <= value <= 10**9):
+                    return False
+            return True
+
+        return False
