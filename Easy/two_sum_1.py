0c520e4c6d4562a4e2ff3ede8d0041c7978494c3
Roman Beskokotov <roma@gmail.com>
2025-06-04 21:34:16 +0300

    Revert "update two sum"
    This reverts commit 5623473478978db2d5571b292c8878c681eae6de.

--- Easy/two_sum_1.py
+++ Easy/two_sum_1.py
@@ -1,14 +1,32 @@
 class Solution:
-    def twoSum(self, nums, target: int):
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]]
-        # If no valid pair is found, return an empty list
-        return []
     
-s = Solution()
-print(s.twoSum([2,7,11,7,15],9))
+    def twoSum(self, nums:list, target: int) -> list[int]:
+        
+        result_sum_index = [] # Список с результирующими индексами для суммы
+
+        if self.check_condition(nums, target):
+            srez = None
+            for i,v in enumerate(nums):
+                if v > target:
+                    srez = i
+                    break
+            
+            if srez and 0 <= srez <= len(nums):
+                nums = nums[:srez]
+
+            for i in range(0, len(nums)):
+                if 0 <= i+1 <= len(nums)-1:
+                    if nums[i] + nums[i+1] == target:
+                        result_sum_index.extend([i, i+1])
+                    
+        return result_sum_index
+
+    def check_condition(self, nums:list, target: int) -> bool:
+        
+        if 2 <= len(nums) <= 10**4 or -10**9 <= target <= 10**9:
+            for index, value in enumerate(nums,1):
+                if not(-10**9 <= value <= 10**9):
+                    return False
+            return True
+
+        return False
